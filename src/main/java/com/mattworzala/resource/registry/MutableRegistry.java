50e5ebd1085f5950105ac8137eadbd0099a5143e
Matt Worzala <mattheworzala@gmail.com>
2021-01-17 16:20:03 -0500

    Add convenience method to create registries.

--- src/main/java/com/mattworzala/resource/registry/MutableRegistry.java
+++ src/main/java/com/mattworzala/resource/registry/MutableRegistry.java
@@ -5,6 +5,10 @@ import com.mattworzala.resource.Resource;
 import org.jetbrains.annotations.NotNull;
 
 public interface MutableRegistry<R extends Resource> extends Registry<R> {
+    static <R extends Resource> MutableRegistry<R> newThreadSafeRegistry() {
+        return new ThreadSafeMapRegistry<>();
+    }
+
 
     void register(@NotNull Identifier id, @NotNull R resource);
 
