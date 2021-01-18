095138006729f3a1cfc56cdbf27b6bc16175412a
Matt Worzala <mattheworzala@gmail.com>
2021-01-17 19:22:35 -0500

    make PostProcessed#postProcess throw JsonParseException

--- src/main/java/com/mattworzala/resource/json/process/PostProcessed.java
+++ src/main/java/com/mattworzala/resource/json/process/PostProcessed.java
@@ -1,5 +1,7 @@
 package com.mattworzala.resource.json.process;
 
+import com.google.gson.JsonParseException;
+
 /**
  *
  */
@@ -9,5 +11,5 @@ public interface PostProcessed {
      * Called after the object has been deserialized, to allow more complex
      * deserialization behavior or checks.
      */
-    void postProcess();
+    void postProcess() throws JsonParseException;
 }
